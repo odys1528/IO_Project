8a47aa653fb8fdcdc2786de51510044419fae86a
odys1528 <jolanta.zakrzewska@orange.pl>
2018-01-06 17:18:51 +0100

    tests for AbbreviationsTransformations

---
+++ src/test/java/pl/put/poznan/transformer/logic/AbbreviationsTransformationsTest.java
@@ -0,0 +1,60 @@
+package pl.put.poznan.transformer.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbbreviationsTransformationsTest {
+    AbbreviationsTransformations a;
+
+    public AbbreviationsTransformationsTest() {
+    }
+
+    @Before
+    public void setUp() {
+        a = new AbbreviationsTransformations();
+    }
+
+    @Test
+    public void test1() {
+        assertEquals("WF", a.abbreviate("WYCHOWANIE FIZYCZNE"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals( "Wf.", a.abbreviate("Wf."));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals("Tj. wf", a.abbreviate("To jest wychowanie fizyczne"));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals("Teraz wf", a.abbreviate("Teraz wychowanie fizyczne"));
+    }
+
+
+    @Test
+    public void test5() {
+        assertEquals("Curriculum vitae ", a.expand("CV "));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals( "doktor", a.expand("dr"));
+    }
+
+    @Test
+    public void test7() {
+        assertEquals("To doktor Kowalski", a.expand("To dr Kowalski"));
+    }
+
+    @Test
+    public void test8() {
+        assertEquals("To znaczy doktor", a.expand("Tzn. dr"));
+    }
+
+}
